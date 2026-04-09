813119a7c0bfa914d32e1f13623877dc219b8ffa
kiinshuk <red2k2green@gmail.com>
2026-04-09 18:12:57 +0530

    Add no-cache headers

--- api/index.js
+++ api/index.js
@@ -55,6 +55,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Cache-Control', 'no-store, must-revalidate');
   if (method === 'OPTIONS') return res.status(200).end();
 
   console.log('Request:', method, path);
